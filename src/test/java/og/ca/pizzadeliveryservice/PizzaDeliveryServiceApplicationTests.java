45e338b87512399ea3c5df928c713ad5e273681e
Višeslav Anđelić <vandelic@ad-control.net>
2025-02-11 17:52:04 +0100

    initial commit

---
+++ src/test/java/og/ca/pizzadeliveryservice/PizzaDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package og.ca.pizzadeliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaDeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
